e518f99325a633185d9a71267a0e8ca7a1278cd8
lukeclement <lclement.57468@farnborough.ac.uk>
2018-01-30 13:30:14 +0000

    Commented parts, kinda ready for use in lab report now

--- graphing.py
+++ graphing.py
@@ -3,15 +3,13 @@ import matplotlib.pyplot as plt
 #plt.style.use('classic')
 plt.style.use('_classic_test')
 x=n.array([5.003,4.944,4.905,4.857,4.804,4.746,4.7,4.653,4.599,4.552,4.498,4.442,4.4,4.355,4.303,4.251,4.2,4.149,4.1,4.054,4.036])
-y=n.array([0.995,0.969,0.882,0.845,0.787,0.747,0.707,0.647,0.602,0.548,0.5,0.45,0.401,0.351,0.3,0.25,0.2,0.15,0.1,0.05,0.04])
-err=n.array([0.018,0.011,0.016,0.023,0.024,0.02,0.015,0.013,0.012,0.012,0.003,0.004,0.001,0.004,0.003,0.001,0.001,0.001,0.001,0.001,0.001])
+y=n.array([0.654,0.712,0.711,0.812,0.882,0.977,1.094,1.254,1.411,1.663,1.924,2.349,2.984,3.738,4.729,6.226,8.444,11.959,17.608,25.488,27.084])
+err=n.array([0.036,0.025,0.024,0.026,0.027,0.036,0.035,0.043,0.056,0.041,0.058,0.042,0.077,0.082,0.083,0.166,0.155,0.298,0.371,0.501,0.48])
+y=1/(n.sqrt(y))
 
-xFit=n.linspace(4,5.05,1000)
-yFit=xFit-4
 
-plt.title("Input frequency vs Output frequency")
+plt.title("Input frequency vs conversion effectiveness")
 plt.xlabel("Input Frequncy/MHz")
-plt.ylabel("Output frequency/MHz")
+plt.ylabel("Effectiveness/%")
 plt.errorbar(x,y,yerr=err,fmt='x')
-plt.plot(xFit,yFit)
 plt.show();
--- main.java
+++ main.java
@@ -6,113 +6,112 @@ import java.io.*;
   Feel free to use, just be sure to cite me! :D
 */
 public class main{
-  public static void main(String[] args){
-    /*
-      Experiment 1
-      Performed on lab day 1, data analysis done on day 2.
-    */
-    //Getting name of file and recording incoming data
-    String fileName="data1.csv";
-    String fNO="pure1.txt";
-    String fNO2="pure2.txt";
-    String line=null;
-    //Values that are needed
-    List<String> iF=new ArrayList<>();
-    List<String> input=new ArrayList<>();
-    List<String> eff=new ArrayList<>();
-    //Reading file
-    try{
-      FileReader fileReader = new FileReader(fileName);
-      BufferedReader br=new BufferedReader(fileReader);
-      while((line=br.readLine())!=null){
-        String[] values=line.split(",");
-        input.add(values[1]);
-        iF.add(values[2]);
-        eff.add(values[5]);
-      }
-      br.close();
-    }catch(Exception ex){System.out.println("File not read");}
-    //Getting values to plot
-    List<String> IFValues=new ArrayList<>();
-    List<String> inputValues=new ArrayList<>();
-    List<String> effValues=new ArrayList<>();
-    List<String> stdI=new ArrayList<>();
-    List<String> stdE=new ArrayList<>();
+    public static void main(String[] args){
+        /*
+         Experiment 1 and 2.
+         Performed on lab day 1, data analysis done on day 2.
+        */
+        //Getting name of file and recording incoming data.
+        String fileName="data1.csv";
+        String fNO="pure1.txt";
+        String fNO2="pure2.txt";
+        String line=null;
+        //Values that are needed.
+        List<String> iF=new ArrayList<>();
+        List<String> input=new ArrayList<>();
+        List<String> eff=new ArrayList<>();
+        //Reading file.
+        try{
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader br=new BufferedReader(fileReader);
+            while((line=br.readLine())!=null){
+                String[] values=line.split(",");
+                input.add(values[1]);
+                iF.add(values[2]);
+                eff.add(values[5]);
+            }
+            br.close();
+        }catch(Exception ex){System.out.println("File not read");}
+        //Getting values to plot.
+        List<String> IFValues=new ArrayList<>();
+        List<String> inputValues=new ArrayList<>();
+        List<String> effValues=new ArrayList<>();
+        List<String> stdI=new ArrayList<>();
+        List<String> stdE=new ArrayList<>();
 
-    for(int i=0;i<21;i++){
-      //Finding mean and stuff
-      Mean meanIF=new Mean(iF,i*10,10);
-      Mean meanI=new Mean(input,i*10,10);
-      Mean meanE=new Mean(eff,i*10,10);
-      IFValues.add(Double.toString(Math.round(meanIF.getMean()*1000)/1000.0));
-      inputValues.add(Double.toString(Math.round(meanI.getMean()*1000)/1000.0));
-      effValues.add(Double.toString(Math.round(meanE.getMean()*1000)/1000.0));
-      stdI.add(Double.toString(Math.round(meanIF.getDev()*1000)/1000.0));
-      stdE.add(Double.toString(Math.round(meanE.getDev()*1000)/1000.0));
-    }
-
-
-    //Writing file 1(pure, experiment 1)
-    try{
-      FileWriter write = new FileWriter(fNO, false);
-      PrintWriter print_line = new PrintWriter(write);
-      for(int i=0;i<stdI.size();i++){
-        print_line.printf( "%s" + "%n" , inputValues.get(i)+" "+IFValues.get(i)+" "+stdI.get(i)+" ");
-      }
-      print_line.close();
-    }catch(Exception ex){}
-    //Writing file 2(pure, experiment 2)
-    try{
-      FileWriter write = new FileWriter(fNO2, false);
-      PrintWriter print_line = new PrintWriter(write);
-      for(int i=0;i<stdI.size();i++){
-        print_line.printf( "%s" + "%n" , inputValues.get(i)+" "+effValues.get(i)+" "+stdE.get(i)+" ");
-      }
-      print_line.close();
-    }catch(Exception ex){}
-
-
-    //Finding anomolies and printing
-    List<Integer> anomolies=new ArrayList<>();
-    anomolies.add(0);
-    int p=0;
-    //while(anomolies.size()>0){
-      anomolies.clear();
-      for(int i=0;i<21;i++){
-        for(int j=0;j<10;j++){
-          double reading=Double.parseDouble(iF.get(i*10+j));
-          double mean=Double.parseDouble(IFValues.get(i));
-          double sigma=Double.parseDouble(stdI.get(i));
-          if((mean-reading)/sigma>2||(mean-reading)/sigma<-2){
-            anomolies.add(i*10+j);
-            System.out.println(i*10+j);
-          }
+        for(int i=0;i<21;i++){
+            //Finding mean and standard deviations.
+            Mean meanIF=new Mean(iF,i*10,10);
+            Mean meanI=new Mean(input,i*10,10);
+            Mean meanE=new Mean(eff,i*10,10);
+            IFValues.add(Double.toString(Math.round(meanIF.getMean()*1000)/1000.0));
+            inputValues.add(Double.toString(Math.round(meanI.getMean()*1000)/1000.0));
+            effValues.add(Double.toString(Math.round(meanE.getMean()*1000)/1000.0));
+            stdI.add(Double.toString(Math.round(meanIF.getDev()*1000)/1000.0));
+            stdE.add(Double.toString(Math.round(meanE.getDev()*1000)/1000.0));
         }
-      }
 
-      for(int a:anomolies){
-        double q=Math.floor(a/10);
-        double target=a-q*10;
-        double sumA=0;
-        double sumB=0;
-        double sumC=0;
-        for(double j=0;j<10;j++){
-          if(j!=target){
-            sumA+=Double.parseDouble(iF.get((int)(j+q*10)));
-            sumB+=Double.parseDouble(input.get((int)(j+q*10)));
-            sumC+=Double.parseDouble(eff.get((int)(j+q*10)));
-          }
+        //Writing file 1(no edits, experiment 1).
+        try{
+            FileWriter write = new FileWriter(fNO, false);
+            PrintWriter print_line = new PrintWriter(write);
+            for(int i=0;i<stdI.size();i++){
+                print_line.printf( "%s" + "%n" , inputValues.get(i)+" "+IFValues.get(i)+" "+stdI.get(i)+" ");
+            }
+            print_line.close();
+        }catch(Exception ex){System.out.println("File not written to");}
+        //Writing file 2(no edits, experiment 2).
+        try{
+            FileWriter write = new FileWriter(fNO2, false);
+            PrintWriter print_line = new PrintWriter(write);
+            for(int i=0;i<stdI.size();i++){
+                print_line.printf( "%s" + "%n" , inputValues.get(i)+" "+effValues.get(i)+" "+stdE.get(i)+" ");
+            }
+            print_line.close();
+        }catch(Exception ex){}
+
+
+        //Finding anomolies and printing to file.
+        List<Integer> anomolies=new ArrayList<>();
+        anomolies.add(0);
+        int p=0;
+        //while(anomolies.size()>0){
+        anomolies.clear();
+        for(int i=0;i<21;i++){
+            for(int j=0;j<10;j++){
+                double reading=Double.parseDouble(iF.get(i*10+j));
+                double mean=Double.parseDouble(IFValues.get(i));
+                double sigma=Double.parseDouble(stdI.get(i));
+                if((mean-reading)/sigma>2||(mean-reading)/sigma<-2){
+                    anomolies.add(i*10+j);
+                    System.out.println(i*10+j);
+                }
+            }
         }
-        //A is IF, B is input
+        //Systematically removing anomolies.
+        for(int a:anomolies){
+            double q=Math.floor(a/10);
+            double target=a-q*10;
+            double sumA=0;
+            double sumB=0;
+            double sumC=0;
+            for(double j=0;j<10;j++){
+            if(j!=target){
+                sumA+=Double.parseDouble(iF.get((int)(j+q*10)));
+                sumB+=Double.parseDouble(input.get((int)(j+q*10)));
+                sumC+=Double.parseDouble(eff.get((int)(j+q*10)));
+            }
+        }
+        //A is IF (Input Frequency), B is input.
         double meanA=sumA/(9);
         double meanC=sumC/(9);
         double x=0;
         double y=0;
         for(double j=0;j<10;j++){
-          if(j!=target){
-            x+=Math.pow(Double.parseDouble(iF.get((int)(j+q*10)))-meanA,2);
-            y+=Math.pow(Double.parseDouble(eff.get((int)(j+q*10)))-meanC,2);
-          }
+            if(j!=target){
+                x+=Math.pow(Double.parseDouble(iF.get((int)(j+q*10)))-meanA,2);
+                y+=Math.pow(Double.parseDouble(eff.get((int)(j+q*10)))-meanC,2);
+            }
         }
         double meanB=sumB/(9);
         double stdIA=Math.pow(x/9.0,0.5);
@@ -124,99 +123,98 @@ public class main{
         effValues.set((int)q,Double.toString(Math.round(meanC*1000)/1000.0));
 
 
-        //Writing files (impure)
-        //Removing first 4 anomolies
+        //Writing files (edited).
+        //Removing first 4 anomolies.
         if(p<4){
+                //Classing each attempt for each experiment and writing to it.
+                String fNOO="E1aAttempt"+Integer.toString(p)+".txt";
+                try{
+                    FileWriter write = new FileWriter(fNOO, false);
+                    PrintWriter print_line = new PrintWriter(write);
+                    for(int i=0;i<stdI.size();i++){
+                        double real=Double.parseDouble(stdI.get(i));
+                        if(real<0.001){
+                            real=0.001;
+                        }
+                        print_line.printf( "%s" + "%n" , inputValues.get(i)+" "+IFValues.get(i)+" "+real+" ");
+                    }
+                    print_line.close();
+                }catch(Exception ex){System.out.println("Problem found \n" +ex);}
+                String fNOOO="E1aAttemptA"+Integer.toString(p)+".txt";
+                try{
+                    File file=new File(fNOOO);
+                    PrintWriter writer = new PrintWriter(new FileOutputStream(file, false));
+                    for(int i=0;i<3;i++){
+                        for(int j=0;j<stdI.size();j++){
+                            double real=Double.parseDouble(stdI.get(j));
+                            if(real<0.001){
+                                real=0.001;
+                            }
+                            switch(i){
+                                case 2:
+                                    writer.write(real+",");
+                                    break;
+                                case 0:
+                                    writer.write(inputValues.get(j)+",");
+                                    break;
+                                case 1:
+                                    writer.write(IFValues.get(j)+",");
+                                    break;
+                            }
+                        }
+                        writer.println(" ");
+                    }
+                    writer.close();
+                }catch(Exception ex){System.out.println("OHNO " +ex);}
 
-          String fNOO="E1aAttempt"+Integer.toString(p)+".txt";
-          try{
-            FileWriter write = new FileWriter(fNOO, false);
-            PrintWriter print_line = new PrintWriter(write);
-            for(int i=0;i<stdI.size();i++){
-              double real=Double.parseDouble(stdI.get(i));
-              if(real<0.001){
-                real=0.001;
-              }
-              print_line.printf( "%s" + "%n" , inputValues.get(i)+" "+IFValues.get(i)+" "+real+" ");
-            }
-            print_line.close();
-          }catch(Exception ex){System.out.println("OHNO");}
-          String fNOOO="E1aAttemptA"+Integer.toString(p)+".txt";
-          try{
-            File file=new File(fNOOO);
-            PrintWriter writer = new PrintWriter(new FileOutputStream(file, false));
-            for(int i=0;i<3;i++){
 
-              for(int j=0;j<stdI.size();j++){
-                double real=Double.parseDouble(stdI.get(j));
-                if(real<0.001){
-                  real=0.001;
+                String fNOO2="E1bAttempt"+Integer.toString(p)+".txt";
+                try{
+                    FileWriter write = new FileWriter(fNOO2, false);
+                    PrintWriter print_line = new PrintWriter(write);
+                    for(int i=0;i<stdI.size();i++){
+                        double real=Double.parseDouble(stdE.get(i));
+                        if(real<0.001){
+                            real=0.001;
+                        }
+                        print_line.printf( "%s" + "%n" , inputValues.get(i)+" "+effValues.get(i)+" "+real+" ");
+                    }
+                    print_line.close();
+                }catch(Exception ex){System.out.println("OHNO");}
+                String fNOOO2="E1bAttemptA"+Integer.toString(p)+".txt";
+                try{
+                  File file=new File(fNOOO2);
+                    PrintWriter writer = new PrintWriter(new FileOutputStream(file, false));
+                    for(int i=0;i<3;i++){
+
+                        for(int j=0;j<stdE.size();j++){
+                            double real=Double.parseDouble(stdE.get(j));
+                            if(real<0.001){
+                                real=0.001;
+                            }
+                            switch(i){
+                                case 2:
+                                    writer.write(real+",");
+                                    break;
+                                case 0:
+                                    writer.write(inputValues.get(j)+",");
+                                    break;
+                                case 1:
+                                    writer.write(effValues.get(j)+",");
+                                    break;
+                            }
+                        }
+                        writer.println(" ");
+                    }
+                    writer.close();
+                }catch(Exception ex){
+                    System.out.println("Problem from line 184-208");
+                    System.out.println(ex);
                 }
-                switch(i){
-                  case 2:
-                  writer.write(real+",");
-                  break;
-                  case 0:
-                  writer.write(inputValues.get(j)+",");
-                  break;
-                  case 1:
-                  writer.write(IFValues.get(j)+",");
-                  break;
-                }
-              }
-              writer.println(" ");
+                
             }
-            writer.close();
-          }catch(Exception ex){System.out.println("OHNO");}
-
-
-          String fNOO2="E1bAttempt"+Integer.toString(p)+".txt";
-          try{
-            FileWriter write = new FileWriter(fNOO2, false);
-            PrintWriter print_line = new PrintWriter(write);
-            for(int i=0;i<stdI.size();i++){
-              double real=Double.parseDouble(stdE.get(i));
-              if(real<0.001){
-                real=0.001;
-              }
-              print_line.printf( "%s" + "%n" , inputValues.get(i)+" "+effValues.get(i)+" "+real+" ");
-            }
-            print_line.close();
-          }catch(Exception ex){System.out.println("OHNO");}
-          String fNOOO2="E1bAttemptA"+Integer.toString(p)+".txt";
-          try{
-            File file=new File(fNOOO2);
-            PrintWriter writer = new PrintWriter(new FileOutputStream(file, false));
-            for(int i=0;i<3;i++){
-
-              for(int j=0;j<stdE.size();j++){
-                double real=Double.parseDouble(stdE.get(j));
-                if(real<0.001){
-                  real=0.001;
-                }
-                switch(i){
-                  case 2:
-                  writer.write(real+",");
-                  break;
-                  case 0:
-                  writer.write(inputValues.get(j)+",");
-                  break;
-                  case 1:
-                  writer.write(effValues.get(j)+",");
-                  break;
-                }
-              }
-              writer.println(" ");
-            }
-            writer.close();
-          }catch(Exception ex){System.out.println("OHNO");}
-
+            //Moving on to the next anomoly.
+            p++;
         }
-
-        p++;
-      }
-      /*
-      */
-
-  }
+    }
 }
